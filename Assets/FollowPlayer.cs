17407535dd5053249aec1aeba0d73c412b817ca3
lcbk0321 <lcbk_0321@dgist.ac.kr>
2019-01-25 21:00:13 +0900

    gumamon is cute

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.position.x < -17.5)
+        {
+            transform.position = new Vector3(-17.5f, 15, -8.5f);
+        }
+        else if(player.position.x >17.5)
+        {
+            transform.position = new Vector3(17.5f, 15, -8.5f);
+        }
+        else {
+            transform.position = new Vector3(player.position.x, 15, -8.5f);
+        }
+        
+    }
+}
