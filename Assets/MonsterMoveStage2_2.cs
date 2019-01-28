ff0b59901bd425a1bbb8fc2473366aa188bfa3ec
Younggyun Brian Yoon <yyg470603@gmail.com>
2019-01-28 16:34:47 +0900

    adsfdads

---
+++ Assets/MonsterMoveStage2_2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMoveStage2_2 : MonoBehaviour
+{
+    public int right = 0;
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (right == 0)
+        {
+            transform.position += new Vector3(0.1f, 0, 0);
+            if (transform.position.x >= 18)
+            {
+                right = 1;
+            }
+
+        }
+
+        if (right != 0)
+        {
+            transform.position -= new Vector3(0.1f, 0, 0);
+            if (transform.position.x <= 4.5)
+            {
+                right = 0;
+            }
+        }
+    }
+}
---
+++ Assets/MonsterMoveStage2_3.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMoveStage2_3 : MonoBehaviour
+{
+    public int right = 0;
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (right == 0)
+        {
+            transform.position += new Vector3(0.08f, 0, 0);
+            if (transform.position.x >= 52.1)
+            {
+                right = 1;
+            }
+
+        }
+
+        if (right != 0)
+        {
+            transform.position -= new Vector3(0.08f, 0, 0);
+            if (transform.position.x <= 40)
+            {
+                right = 0;
+            }
+        }
+    }
+}
